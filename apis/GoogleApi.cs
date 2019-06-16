f7f12e30ca5ea581b679788de31552215ee663d9
Nathan Button <nate601@live.com>
2019-06-16 01:34:27 -0500

    Begin parsing GoogleApi, add tools for said parsing

--- apis/GoogleApi.cs
+++ apis/GoogleApi.cs
@@ -1,7 +1,23 @@
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
 namespace backend.Apis
 {
-    static class GoogleApi
+    public static class GoogleApi
     {
+        public static GoogleBookInfo GetBookInfo(int Isbn)
+        {
+            var client = new RestClient(@"https://www.googleapis.com/books/v1");
+            var searchRequest = new RestRequest("volumes?=searchTerm");
+            searchRequest.AddUrlSegment("searchTerms", $"isbn:{Isbn.ToString()}");
+            IRestResponse response = client.Execute(searchRequest, Method.GET);
+            JObject content = JObject.Parse(response.Content);
+            JToken authorNameToken = content.SelectToken("$.items[0].volumeInfo.title");
+
+
+            return null; //TODO:Remove
+        }
         public class GoogleBookInfo
         {
             public string AuthorName;
