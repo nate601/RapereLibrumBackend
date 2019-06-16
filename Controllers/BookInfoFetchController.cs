95edc9ea36def6c57acbd7e1f081840bd7c2309a
Nathan Button <nate601@live.com>
2019-06-16 02:16:00 -0500

    Implement fully BookInfoFetchController along with the GoogleApi Backend

--- Controllers/BookInfoFetchController.cs
+++ Controllers/BookInfoFetchController.cs
@@ -7,7 +7,16 @@ namespace backend.Controllers
     [ApiController]
     public class BookInfoFetchController : ControllerBase
     {
-
+        [HttpGet("{Isbn}")]
+        public ActionResult<backend.Apis.GoogleApi.GoogleBookInfo> GetBookInfo(string Isbn)
+        {
+            Apis.GoogleApi.GoogleBookInfo value = backend.Apis.GoogleApi.GetBookInfo(Isbn);
+            if (value == null)
+            {
+                return new NotFoundResult();
+            }
+            return new OkObjectResult(value);
+        }
     }
 
 }
--- apis/GoogleApi.cs
+++ apis/GoogleApi.cs
@@ -6,28 +6,49 @@ namespace backend.Apis
 {
     public static class GoogleApi
     {
-        public static GoogleBookInfo GetBookInfo(int Isbn)
+        public static GoogleBookInfo GetBookInfo(string Isbn)
         {
-            var client = new RestClient(@"https://www.googleapis.com/books/v1");
-            var searchRequest = new RestRequest("volumes?=searchTerm");
-            searchRequest.AddUrlSegment("searchTerms", $"isbn:{Isbn.ToString()}");
-            IRestResponse response = client.Execute(searchRequest, Method.GET);
-            JObject content = JObject.Parse(response.Content);
-            JToken authorNameToken = content.SelectToken("$.items[0].volumeInfo.title");
-            JToken bookTitle = content.SelectToken("$.items[0].volumeTitle.authors[0]");
-            return new GoogleBookInfo(authorNameToken.ToString(), bookTitle.ToString(), Isbn.ToString());
+            try
+            {
+
+                var client = new RestClient(@"https://www.googleapis.com");
+                var searchRequest = new RestRequest("/books/v1/volumes?q=isbn:" + Isbn);
+                IRestResponse response = client.Execute(searchRequest, Method.GET);
+                if (!response.IsSuccessful || response.StatusCode != System.Net.HttpStatusCode.OK)
+                {
+                    return null;
+                }
+                JObject content = JObject.Parse(response.Content);
+                JToken authorNameToken = content.SelectToken("$.items[0].volumeInfo.authors[0]");
+                JToken bookTitle = content.SelectToken("$.items[0].volumeInfo.title");
+                JToken urlToThumbnail = content.SelectToken("$.items[0].volumeInfo.imageLinks.thumbnail");
+                JToken averageRating = content.SelectToken("$.items[0].volumeInfo.averageRating");
+                JToken numberOfRatings = content.SelectToken("$.items[0].volumeInfo.ratingsCount");
+                return new GoogleBookInfo(authorNameToken.Value<String>(), bookTitle.Value<String>(), Isbn.ToString(), urlToThumbnail.Value<String>(), averageRating.Value<String>(), numberOfRatings.Value<String>());
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
         public class GoogleBookInfo
         {
             public string AuthorName;
             public string BookTitle;
             public string Isbn;
+            public string UrlToThumbnail;
+            public string AverageRating;
+            public string NumberOfRatings;
 
-            public GoogleBookInfo(string _AuthorName, string _BookTitle, string _Isbn)
+            public GoogleBookInfo(string _AuthorName, string _BookTitle, string _Isbn, string _UrlToThumbnail, string _averageRating, string _numberOfRatings)
             {
                 this.AuthorName = _AuthorName;
                 this.BookTitle = _BookTitle;
                 this.Isbn = _Isbn;
+                this.UrlToThumbnail = _UrlToThumbnail;
+                this.AverageRating = _averageRating;
+                this.NumberOfRatings = _numberOfRatings;
             }
         }
     }
