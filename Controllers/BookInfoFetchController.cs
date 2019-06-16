8cc70def28f1d38990eee15c6d0fa0ed88eccc3e
Nathan Button <nate601@live.com>
2019-06-16 01:06:53 -0500

    Initial Commit

---
+++ Controllers/BookInfoFetchController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookInfoFetchController : ControllerBase
+    {
+
+    }
+
+}
