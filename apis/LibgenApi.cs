8ff49f8805c27022e50572bc026f33082f248125
Nathan Button <nate601@live.com>
2019-06-16 22:39:01 -0500

    Implement Libgen Api class

--- apis/LibgenApi.cs
+++ apis/LibgenApi.cs
@@ -1,7 +1,47 @@
+using System;
+using System.Collections.Generic;
+using ScrapySharp;
+using ScrapySharp.Network;
+using ScrapySharp.Extensions;
+using HtmlAgilityPack;
+using System.Linq;
+using ScrapySharp.Html;
+
 namespace backend.Apis
 {
-    public class LibgenApi
+    public static class LibgenApi
     {
-        
+        //  http://libgen.io/search.php?req=Mistborn
+        public static RowInfo[] GetLibgen(string bookTitle)
+        {
+            ScrapingBrowser browser = new ScrapingBrowser();
+            var page = browser.NavigateToPage(new Uri($"http://libgen.io/search.php?req={bookTitle}"));
+            var rowHtml = page.Html.CssSelect("body > table.c > tbody > tr:nth-child(n+2)");
+            var rowHtmlEpub = rowHtml.Where((node) => node.ToString().Contains("epub"));
+            var rowInfos = rowHtmlEpub.Select((node) =>
+            {
+                return new RowInfo(node);
+            });
+            return rowInfos.ToArray();
+
+        }
+        public class RowInfo
+        {
+            string idNumber;
+            string authorName;
+            string bookTitle;
+            string[] mirrorLinks;
+            string fileSize;
+
+            public RowInfo(HtmlNode rowHtml)
+            {
+                this.idNumber = rowHtml.ChildNodes[0].InnerText;
+                this.authorName = rowHtml.ChildNodes[1].FirstChild.InnerText;
+                this.bookTitle = rowHtml.CssSelect(".title").First().InnerText;
+                this.fileSize = rowHtml.CssSelect("td:nth-last-child(4)").First().InnerText;
+                this.mirrorLinks = rowHtml.CssSelect("tbody > tr").CssSelect("a[href]").Select((node) => { return node.GetAttributeValue("href"); }).ToArray();
+            }
+        }
+
     }
 }
